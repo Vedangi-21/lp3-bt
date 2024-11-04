4796cbfabe564634de4f5954f5708f580e3d41ef
Vedangi-21 <114250307+Vedangi-21@users.noreply.github.com>
2024-11-04 22:31:49 +0530

    Update student.sol

--- student.sol
+++ student.sol
@@ -1,11 +1,7 @@
-// SPDX-License-Identifier: MIT   
 pragma solidity >= 0.8.7;
 
-// Build the Contract
 contract MarksManagmtSys
 {
-	// Create a structure for
-	// student details
 	struct Student
 	{
 		int ID;
@@ -28,23 +24,16 @@ contract MarksManagmtSys
 		owner=msg.sender;
 	}
 
-	// Create a function to add
-	// the new records
 	function addNewRecords(int _ID,
 						string memory _fName,
 						string memory _lName,
 						int _marks) public onlyOwner
 	{
-		// Increase the count by 1
 		stdCount = stdCount + 1;
 
-		// Fetch the student details
-		// with the help of stdCount
-		stdRecords[stdCount] = Student(_ID, _fName,
-									_lName, _marks);
+		stdRecords[stdCount] = Student(_ID, _fName, _lName, _marks);
 	}
 
-	// Create a function to add bonus marks
 	function bonusMarks(int _bonus) public onlyOwner
 	{
 		stdRecords[stdCount].marks =
